255476dad11a71c7a9d9dddef2850c908592b775
cervoliu <cervol@foxmail.com>
2024-10-24 12:36:49 +0800

    small fixes

--- eqRunner.py
+++ eqRunner.py
@@ -2,9 +2,8 @@ import os
 from checker.equivalenceChecker import check_program_equivalence
 
 if __name__ == "__main__":
-
     clang_path = "~/.local/llvm-13/bin/clang"
-    
+
     klee_source_path = "~/klee/klee_fork"
     klee_build_path = "~/klee/klee_build"
 
@@ -12,10 +11,7 @@ if __name__ == "__main__":
     klee_exe_path = klee_build_path + "/bin/klee"
 
     sourceA_path, sourceB_path = None, None
-    # sourceA_path = "~/klee/klee_fork/examples/equivalence_check/2/A/A.c"
-    # sourceB_path = "~/klee/klee_fork/examples/equivalence_check/2/B/B.c"
-    sourceA_path = "/home/lyd24/benchmarks/CLEVER/divide/Eq/newV.c"
-    sourceB_path = "/home/lyd24/benchmarks/CLEVER/divide/Eq/oldV.c"
+
     if sourceA_path is None:
         sourceA_path = input("Enter the path to the first source file: ")
     if sourceB_path is None:
@@ -31,7 +27,7 @@ if __name__ == "__main__":
     # clean tmp directory
     os.system(f"rm -r {tmp}")
     os.makedirs(tmp)
-        
+
     # compile source
 
     compile_args = f"-I {klee_include_path} -emit-llvm -c -g -O0 -Xclang -disable-O0-optnone"
@@ -39,10 +35,31 @@ if __name__ == "__main__":
     os.system(f"{clang_path} {compile_args} {sourceB_path} -o {tmp}/B.bc")
 
     # run klee on bitcode
-    klee_options = f"-posix-runtime"
-    program_options = f""
-    os.system(f"{klee_exe_path} {klee_options} --output-dir={tmp}/A {tmp}/A.bc {program_options}")
-    os.system(f"{klee_exe_path} {klee_options} --output-dir={tmp}/B {tmp}/B.bc {program_options}")
+    klee_constraint_solving_options = "--solver-backend=z3 --use-query-log=solver:kquery "
+    klee_debugging_options = " "
+    klee_execution_tree_options = "--write-exec-tree --write-sym-paths "
+    klee_expression_options = " "
+    klee_external_call_policy_options = " "
+    klee_linking_options = " "
+    klee_memory_management_options = " "
+    klee_test_case_options = "--write-no-tests "
+
+    klee_options = (
+        klee_constraint_solving_options
+        + klee_debugging_options
+        + klee_execution_tree_options
+        + klee_expression_options
+        + klee_external_call_policy_options
+        + klee_linking_options
+        + klee_memory_management_options
+        + klee_test_case_options
+    )
+
+    program_args = ""
+    redirect_output = " > /dev/null 2>&1"
+
+    os.system(f"{klee_exe_path} {klee_options} --output-dir={tmp}/A {tmp}/A.bc {program_args} {redirect_output}")
+    os.system(f"{klee_exe_path} {klee_options} --output-dir={tmp}/B {tmp}/B.bc {program_args} {redirect_output}")
 
     # check equivalence
     print("Equivalence" if check_program_equivalence(f"{tmp}/A", f"{tmp}/B") else "Not Equivalent")
